f928c1fdc5537be29adbd0f7b1b9958183cf80f5
KimYangHyeon <xiu9003260@gmail.com>
2023-05-07 13:02:55 +0900

    Initial Commit

---
+++ Wemo_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wemo_
+//
+//  Created by 김양현 on 2023/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
